d4d8e0cc1c3fd5d509e1e73ae244e1e72d334914
Harshith reddy <harshith@nothr.in>
2024-10-03 09:34:41 +0530

    feat(pages): Added pages support

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import starlight from "@astrojs/starlight";
 
 // https://astro.build/config
 export default defineConfig({
+  site:'cogaan-club-gitam.github.io',
+  base:'wiki',
   integrations: [
     starlight({
       title: "Cogaan Club Wiki",
